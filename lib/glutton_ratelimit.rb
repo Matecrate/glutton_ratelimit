f79d6fa9e8190bf737311a3272919989cc0abe18
Benjamin Behr <benny@digitalbehr.de>
2018-07-26 10:07:25 +0200

    remove deprecation warning

--- lib/glutton_ratelimit.rb
+++ lib/glutton_ratelimit.rb
@@ -23,7 +23,7 @@ module GluttonRatelimit
     
     def times(num, &block)
       raise ArgumentError, "Code block expected"  if not block
-      raise ArgumentError, "Parameter expected to be Fixnum but found a #{num.class}."  unless num.class.equal?(Fixnum)
+      raise ArgumentError, "Parameter expected to be Integer but found a #{num.class}."  unless num.class.equal?(Integer)
       num.times do
         wait
         yield
